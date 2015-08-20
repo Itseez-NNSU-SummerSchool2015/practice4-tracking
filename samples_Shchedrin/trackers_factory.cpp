22d8fdaf2a081f02602cfcc10de7233cbe7031f7
rashchedrin <ss2015@ws-2k-110-n11.ccam.cmc.unn.net>
2015-08-20 14:12:18 +0300

    implementing tracker

--- samples_Shchedrin/trackers_factory.cpp
+++ samples_Shchedrin/trackers_factory.cpp
@@ -1,16 +1,14 @@
 #include "tracker.hpp"
 
 cv::Ptr<Tracker> createTrackerDummy();
-// TODO: Declare your implementation here
-// cv::Ptr<Tracker> createTrackerYourName();
+cv::Ptr<Tracker> createTrackerShchedrin();
 
 cv::Ptr<Tracker> createTracker(const std::string &impl_name)
 {
     if (impl_name == "dummy")
         return createTrackerDummy();
-    // TODO: Add case for your implementation
-    // else if (impl_name == "your_name"):
-    //     return createTrackerYourName();
+    else if (impl_name == "Shchedrin")
+        return createTrackerShchedrin();
 
     return 0;
 }
